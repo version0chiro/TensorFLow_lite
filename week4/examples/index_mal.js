fc1a5acf7681aa8162ea109339e615b70a21ba37
Sachin Singh Bhadoriya <56084650+version0chiro@users.noreply.github.com>
2020-09-18 18:14:43 +0530

    added function to handle buttons

--- week4/examples/index_mal.js
+++ week4/examples/index_mal.js
@@ -28,4 +28,26 @@ async function train(){
     ]
   });
 }
+
+function handleButton(elem){
+	switch(elem.id){
+		case "0":
+			rockSamples++;
+			document.getElementById("rocksamples").innerText = "Rock samples:" + rockSamples;
+			break;
+		case "1":
+			paperSamples++;
+			document.getElementById("papersamples").innerText = "Paper samples:" + paperSamples;
+			break;
+		case "2":
+			scissorsSamples++;
+			document.getElementById("scissorssamples").innerText = "Scissors samples:" + scissorsSamples;
+			break;
+	}
+	label = parseInt(elem.id);
+	const img = webcam.capture();
+	dataset.addExample(mobilenet.predict(img), label);
+
+}
+
 init();
